7f99d1af29773ee30d132abea41ac565c5836a43
Marta <maslankowska.marta@gmail.com>
2019-11-07 20:22:01 +0100

    Plotting methods added for single image - showing image and histogram.

--- data_loader/utils.py
+++ data_loader/utils.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 from PIL import Image
 from settings import DATA_PATH
+import matplotlib.pyplot as plt
 
 
 def load_image_as_norm_array(file_path):
@@ -15,7 +16,7 @@ def load_image_as_norm_array(file_path):
     path = os.path.join(DATA_PATH, file_path)
     img = Image.open(path)
     img.load()
-    data = np.asarray(img, dtype="int32")
+    data = np.asarray(img, dtype=np.uint8)
     data = data / 255.0   # normalization
     return data
 
@@ -29,16 +30,17 @@ def load_image_as_array(file_path):
     path = os.path.join(DATA_PATH, file_path)
     img = Image.open(path)
     img.load()
-    data = np.asarray(img, dtype="int32")
+    data = np.asarray(img, dtype=np.uint8)
     return data
 
 
 def load_image_by_cv2(file_path):
     """
-    Funtion load single image from file using cv2 method.
+    Function load single image from file using cv2 method. Probably still loads image in BGR...
     :param file_path:
     :return: image as numpy array
     """
     path = os.path.join(DATA_PATH, file_path)
     img = cv2.imread(path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return img
---
+++ visualization/single_image_plotter.py
@@ -0,0 +1,64 @@
+import os
+import cv2
+from matplotlib import pyplot as plt
+# import seaborn as sns
+
+from user_settings import DATA_PATH
+
+
+class SingleImagePlotter():
+    """
+    Shows plots connected with given image.
+    """
+    def __init__(self):
+        pass
+
+    def plot_image_from_path(self, directory, img_name, fig_size=10):
+        """
+        Loads image from given path and shows it.
+        :param directory: directory containing specific image type eg. photo, cartoon
+        :param img_name: image name including extension
+        """
+        path = os.path.join(DATA_PATH, directory)
+        img_path = os.path.join(path, img_name)
+
+        img = cv2.imread(img_path)
+        image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        plt.figure(figsize=(fig_size, fig_size))
+        plt.axis("off")
+        plt.imshow(image)
+        plt.show()
+
+    def plot_image(self, image, fig_size=10):
+        """
+        Simply plots image.
+        :param image: image as numpy array
+        """
+        plt.figure(figsize=(fig_size, fig_size))
+        plt.axis("off")
+        plt.imshow(image)
+        plt.show()
+
+    def plot_image_hist(self, image):
+        """
+        Shows histogram of image pixels.
+        :param image: image as numpy array
+        """
+        histr = cv2.calcHist([image], [0], None, [256], [0, 256])
+        plt.plot(histr)
+        plt.xlim([0, 256])
+        plt.show()
+
+    def plot_image_hist_rgb(self, image):
+        """
+        Shows coloured histogram of image pixels for each RGB chanel.
+        :param image: image as numpy array
+        """
+        color = ('b', 'g', 'r')
+        for i, col in enumerate(color):
+            histr = cv2.calcHist([image], [i], None, [256], [0, 256])
+            plt.plot(histr, color=col)
+            plt.xlim([0, 256])
+        plt.show()
+
