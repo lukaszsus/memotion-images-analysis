a61b60d0c60e32a5f204e24e7e68c5e0262e7804
Marta <maslankowska.marta@gmail.com>
2019-12-03 10:05:35 +0100

    Added some methods for running all classifiers at once on multiple files.

--- classifiers/tests_for_multiple_classifiers.py
+++ classifiers/tests_for_multiple_classifiers.py
@@ -69,7 +69,7 @@ def sklearn_mlp_metrics(x, y, x_labels, y_labels, hn=(256, ), activation='relu')
 
 def plot_table_of_metrics(classifiers_metrics):
     table = pd.DataFrame(np.array(classifiers_metrics), columns=['Classfier', 'Accuracy', 'F-score'])
-    plt.figure(figsize=(6, 2))
+    plt.figure(figsize=(7, 2))
 
     cell_text = []
     for row in range(len(table)):
@@ -80,14 +80,61 @@ def plot_table_of_metrics(classifiers_metrics):
     plt.show()
 
 
-if __name__ == "__main__":
-    filename = 'pics_bilateral_50_40'
-    x, y, x_labels, y_labels = load_dataset(filename)
+def run_all_classifiers(x, y, x_labels, y_labels,
+                        max_depth=3, min_samples_leaf=5, estimators=15, n_neighbours=3, hn=(256, ), activation='relu'):
+    """
+    Method that runs all classifiers and returns numpy array with accuracy and f-score for each (with classifier name).
+    :param x: features values read from file
+    :param y: label values (from 0 to 3) read from file
+    :param x_labels: feature names
+    :param y_labels: label names - cartoon, painting, photo and text
+    :param max_depth: CART clf - maximum tree depth
+    :param min_samples_leaf: CART clf - minimum number of samples in one leaf
+    :param estimators: RandomForest clf - number of weak classifiers/trees
+    :param n_neighbours: kNN clf - number of nearest neighbours
+    :param hn: MLP clf - number of hidden layers (as a tuple)
+    :param activation: MLP clf - activation function
+    :return: numpy array of (clf_name, accuracy, f-score)
+    """
 
     naive_bayes = naive_bayes_metrics(x, y, x_labels, y_labels)
-    trees = decission_tree_metrics(x, y, x_labels, y_labels)
-    knn = knn_metrics(x, y, x_labels, y_labels)
-    nn = sklearn_mlp_metrics(x, y, x_labels, y_labels)
+    trees = decission_tree_metrics(x, y, x_labels, y_labels, max_depth, min_samples_leaf, estimators)
+    knn = knn_metrics(x, y, x_labels, y_labels, n_neighbours)
+    nn = sklearn_mlp_metrics(x, y, x_labels, y_labels, hn, activation)
     classifiers_metrics = np.array(naive_bayes + trees + knn + nn)
+    classifiers_metrics[:, 1:] = np.around(np.array(classifiers_metrics[:, 1:], dtype=np.float32) * 100, 2)
+    return classifiers_metrics
 
+
+def load_multiple_datasets(filenames):
+    x_features, x_names = [], []
+    for filename in filenames:
+        x, y, x_labels, y_labels = load_dataset(filename)
+        x_features.append(np.array(x))
+        x_names += [f'{x_label}_{filename}' for x_label in x_labels]
+
+    x_features = np.reshape(x_features, (-1, len(x_names)))
+    df = pd.DataFrame(np.concatenate((np.array(x_features), np.array(y).reshape(-1, 1)), axis=1),
+                      columns=x_names+['label'])
+
+    return df, y_labels
+
+
+def load_x_y_from_files(filenames):
+    """
+    Method loading data from multiple .pkl files - changing dataframe to x and y (suitable for classifiers).
+    :param filenames: list of filenames from datasets_pkl directory to load
+    :return: array of x values concatenated for all files, array of y labels and feature names
+    """
+    df, y_labels = load_multiple_datasets(filenames)
+    vals = df.values
+    x_labels = df.columns[:-1]
+    return vals[:, :-1], np.array(vals[:, -1], dtype=np.int32), x_labels, y_labels
+
+
+if __name__ == "__main__":
+    filenames = ['pics_bilateral_30_50']  # ['pics_bilateral_30_50', 'pics_bilateral_50_40']
+    x, y, x_labels, y_labels = load_x_y_from_files(filenames)
+
+    classifiers_metrics = run_all_classifiers(x, y, x_labels, y_labels)
     plot_table_of_metrics(classifiers_metrics)
