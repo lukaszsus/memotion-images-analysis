9d1f04b466dafd358af408829f084b374a28daf8
Marta <maslankowska.marta@gmail.com>
2019-12-29 23:18:24 +0100

    Adjusting min_lines in Hough Transformation - experiments.

--- feature_extraction/color_counter.py
+++ feature_extraction/color_counter.py
@@ -16,3 +16,15 @@ class ColorCounter():
         n_colors = np.unique(im, axis=0).shape[0]
         factor = n_colors / n_pix
         return factor
+
+    def norm_color_count_without_white_and_black(self, image):
+        """
+        Counts number of colors divided by number of pixels in picture
+        excluding pixels that are white and black - first and last color.
+        """
+        im = image.reshape((-1, 3))
+        colors, counts = np.unique(im, axis=0, return_counts=True)
+        n_pix = np.sum(counts[1:-1])
+        n_colors = colors.shape[0] - 2  # without first and last
+        factor = n_colors / n_pix
+        return factor
--- image_segmentation/hough_lines.py
+++ image_segmentation/hough_lines.py
@@ -5,46 +5,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from data_loader.utils import load_image_by_cv2
+from feature_extraction.color_counter import ColorCounter
 
 
 class HoughLines:
     def __init__(self):
         pass
 
-    def plot_lines(self, img, min_line_len=150):
+    def get_image_with_lines(self, img, min_line_len=150, plot=False):
         gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
         edges = cv2.Canny(gray, 50, 150, apertureSize=3)
 
-        plt.figure(figsize=(5, 10))
-        plt.imshow(edges, cmap='gray')
-        plt.xticks([])
-        plt.yticks([])
-        plt.show()
+        if plot:
+            # plt.figure(figsize=(5, 10))
+            plt.imshow(edges, cmap='gray')
+            plt.axis('off')
+            plt.show()
 
         image = img.copy()
 
         lines = cv2.HoughLines(edges, 1, np.pi / 180, min_line_len)
-        for line in lines:
-            rho, theta = line[0]
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a * rho
-            y0 = b * rho
-            x1 = int(x0 + 1000 * (-b))
-            y1 = int(y0 + 1000 * (a))
-            x2 = int(x0 - 1000 * (-b))
-            y2 = int(y0 - 1000 * (a))
-            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 5)
-            cv2.line(image, (x1, y1), (x2, y2), (255, 255, 255), 10)
+        # print(f'Lines: {len(lines)}')
+        if lines is not None:
+            for line in lines:
+                rho, theta = line[0]
+                a = np.cos(theta)
+                b = np.sin(theta)
+                x0 = a * rho
+                y0 = b * rho
+                x1 = int(x0 + 1000 * (-b))
+                y1 = int(y0 + 1000 * (a))
+                x2 = int(x0 - 1000 * (-b))
+                y2 = int(y0 - 1000 * (a))
+                cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 5)
+                cv2.line(image, (x1, y1), (x2, y2), (255, 255, 255), 10)
 
-        plt.figure(figsize=(5, 10))
-        plt.imshow(img, cmap='gray')
-        plt.axis('off')
-        plt.show()
+            if plot:
+                # plt.figure(figsize=(5, 10))
+                plt.imshow(img, cmap='gray')
+                plt.axis('off')
+                plt.show()
 
-        return image
+        return image, int(np.sum(edges) / 255) / (edges.shape[0] * edges.shape[1])
 
-    def get_bounding_boxes(self, img):
+    def get_bounding_boxes(self, img, plot=False):
         size = list(img.shape[:2])
         size.reverse()  # get a [width,height] list
 
@@ -66,7 +70,6 @@ class HoughLines:
 
         # Get panels out of contours
         for contour in contours:
-
             arclength = cv2.arcLength(contour, True)
 
             epsilon = 0.01 * arclength
@@ -75,7 +78,7 @@ class HoughLines:
             x, y, w, h = cv2.boundingRect(approx)
 
             # exclude very small panels
-            if w < infos['size'][0] / 15 or h < infos['size'][1] / 15:  # or approx.shape[0] > 4:
+            if w < infos['size'][0] / 15 or h < infos['size'][1] / 15 or approx.shape[0] > 4:
                 # print('...panel excluded\n')
                 continue
 
@@ -97,19 +100,38 @@ class HoughLines:
                 'h': h
             }
 
-        plt.figure(figsize=(5, 10))
-        plt.imshow(img)
-        plt.axis('off')
-        plt.show()
+        if plot:
+            # plt.figure(figsize=(5, 10))
+            plt.imshow(img)
+            plt.axis('off')
+            plt.show()
 
         return infos['panels']
 
 
 if __name__ == "__main__":
-    file_name = 'kermit.jpg'
-    file_path = os.path.join("base_dataset", "segmentation", file_name)
-    image = load_image_by_cv2(file_path)
+    from time import time
+    file_names = ['drake', 'cartoon_3', 'cat', 'kermit', 'none_5', 'stats']
+    labels = [2, 3, 2, 1, 1, 3]
 
-    hl = HoughLines()
-    image = hl.plot_lines(image, 250)
-    hl.get_bounding_boxes(image)
+    for i, file_name in enumerate(file_names):
+        # file_name = 'drake.jpg'
+        file_path = os.path.join("base_dataset", "segmentation", file_name + '.jpg')
+        loaded_image = load_image_by_cv2(file_path)
+        x, y, _ = loaded_image.shape
+        cc = ColorCounter()
+
+        print(x, y)
+        # print(cc.norm_color_count(loaded_image))
+        # print(cc.norm_color_count_without_white_and_black(loaded_image))
+
+        hl = HoughLines()
+        lines = [120, 150, 180, 200, 250, 300, 350, 400, 450, 500]
+        # lines = [100]
+        # lines = np.array(np.linspace(int(upper_border/3), int(upper_border/2), 10), dtype=np.int32)
+        print('\n\n-------------------\n', file_name.upper())
+        for l in lines:
+            print(f'\nMinimum no of points: {l}')
+            im, norm_edges = hl.get_image_with_lines(loaded_image.copy(), l, plot=False)
+            print(f'Normalized edges: {round(norm_edges, 4)}')
+            hl.get_bounding_boxes(im, plot=False)
